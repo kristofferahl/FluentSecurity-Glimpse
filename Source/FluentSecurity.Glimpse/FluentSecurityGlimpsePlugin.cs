be4d09cfe00bf6b85c937d46f29ca11f6009d5b3
Kristoffer Ahl <git@77dynamite.com>
2011-08-26 11:00:11 +0200

    Setup project for plugin. Included packages. Created a very basic glimpse plugin.

---
+++ Source/FluentSecurity.Glimpse/FluentSecurityGlimpsePlugin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glimpse.Core.Extensibility;
+
+namespace FluentSecurity.Glimpse
+{
+	[GlimpsePlugin]
+	public class FluentSecurityGlimpsePlugin : IGlimpsePlugin
+	{
+		public object GetData(HttpContextBase context)
+		{
+			var data = new List<object[]> { new[] { "Controller", "Action", "Policies" } };
+
+			var configuration = SecurityConfiguration.Current;
+			if (configuration != null)
+			{
+				foreach (var policyContainer in configuration.PolicyContainers)
+					data.Add(new object[] { policyContainer.ControllerName, policyContainer.ActionName, policyContainer.GetPolicies().Select(x => x.GetType().Name).ToArray() });
+			}
+
+			return data;
+		}
+
+		public void SetupInit() {}
+
+		public string Name
+		{
+			get { return "Fluent Security"; }
+		}
+	}
+}

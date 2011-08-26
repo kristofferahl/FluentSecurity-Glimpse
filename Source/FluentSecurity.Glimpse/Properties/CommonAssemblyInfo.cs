b7b9f79839752d60a5f6fc1b449d552c59606f15
Kristoffer Ahl <git@77dynamite.com>
2011-08-26 23:03:59 +0200

    Fixed the assembly info for the project.

---
+++ Source/FluentSecurity.Glimpse/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyVersionAttribute("1.0.0.0000")]
+[assembly: AssemblyCopyrightAttribute("Copyright (c) 2011, Kristoffer Ahl")]
+[assembly: AssemblyProductAttribute("FluentSecurity.Glimpse")]
+[assembly: AssemblyTitleAttribute("FluentSecurity.Glimpse")]
+[assembly: AssemblyCompanyAttribute("")]
+[assembly: AssemblyConfigurationAttribute("release")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.0.0000")]
+[assembly: AssemblyFileVersionAttribute("1.0.0.0000")]
+

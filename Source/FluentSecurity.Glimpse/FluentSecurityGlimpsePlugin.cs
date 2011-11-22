3c3eef477545d1920d8562d157400388ea87c9cb
Kristoffer Ahl <git@77dynamite.com>
2011-11-22 19:05:22 +0100

    Added a massive todo-list for the plugin.

--- Source/FluentSecurity.Glimpse/FluentSecurityGlimpsePlugin.cs
+++ Source/FluentSecurity.Glimpse/FluentSecurityGlimpsePlugin.cs
@@ -14,6 +14,28 @@ namespace FluentSecurity.Glimpse
 			var configuration = GetSecurityConfiguration();
 			if (configuration != null)
 			{
+				// TODO: See below...
+				// * Current version of the loaded Fluent Security assembly
+				// * Link to documentation
+				// * Current version of Fluent Security (Link to NuGet)
+				// * Ignore missing configuration
+				// * Service locator specified + type
+				// * Current configuration events (what was the path that led to the current configurtion)
+				// * Current configuration (policies)
+				// * Request details (what did Fluent Security do during the most recent request)
+				//		- What was the controller and action that was called
+				//		- How many policies was in the container that matched that controller action
+				//		- How many policies executed
+				//		- Which policies executed
+				//		- How long did it take to execute them
+				//		- What was the result of each policy
+				//		- What did Fluent Security do with that result
+				//		- What violation handler was selected
+				//		- How was that violation handler selected
+				//		- What was the result of that violation handler
+				//		- How long did it take to execute that violation handler
+				//		- ...
+
 				//var topLevel = new GlimpseRoot();
 				//topLevel.AddRow().Column("Key").Column("Value");
 

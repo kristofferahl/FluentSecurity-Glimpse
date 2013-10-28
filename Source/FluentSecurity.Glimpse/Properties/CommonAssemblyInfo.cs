f1bba64c0c2c9e4272efab11d4db89dea9bd9841
Kristoffer Ahl <git@77dynamite.com>
2013-10-28 21:13:19 +0100

    - Bumped version number to 2.0.0.

--- Source/FluentSecurity.Glimpse/Properties/CommonAssemblyInfo.cs
+++ Source/FluentSecurity.Glimpse/Properties/CommonAssemblyInfo.cs
@@ -2,12 +2,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyVersionAttribute("1.1.0.0000")]
+[assembly: AssemblyVersionAttribute("2.0.0")]
 [assembly: AssemblyCopyrightAttribute("Copyright (c) 2011, Kristoffer Ahl")]
 [assembly: AssemblyProductAttribute("FluentSecurity.Glimpse")]
 [assembly: AssemblyTitleAttribute("FluentSecurity.Glimpse")]
 [assembly: AssemblyCompanyAttribute("")]
 [assembly: AssemblyConfigurationAttribute("release")]
-[assembly: AssemblyInformationalVersionAttribute("1.1.0.0000")]
-[assembly: AssemblyFileVersionAttribute("1.1.0.0000")]
-
+[assembly: AssemblyInformationalVersionAttribute("2.0.0")]
+[assembly: AssemblyFileVersionAttribute("2.0.0")]

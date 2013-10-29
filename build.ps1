05d1a9f6d5a46fcd2e2a107ccd3e4e127ed2bc89
Kristoffer Ahl <git@77dynamite.com>
2013-10-29 15:31:52 +0100

    - Copied parts of the build script from FluentSecurity to make the info section better.

--- build.ps1
+++ build.ps1
@@ -1,18 +1,20 @@
 properties {
 	$product		= 'FluentSecurity.Glimpse'
 	$version		= '2.0.0'
+	$label			= ''
 	$configuration	= 'release'
 	$useVerbose		= $false
+
+	$artifactsName	= "$product-$version-$configuration" -replace "\.","_"
 
 	$rootDir		= '.'
 	$sourceDir		= "$rootDir\Source"
 	$buildDir		= "$rootDir\Build"
-	$artifactsDir	= "$buildDir\Artifacts"
-	$artifactsName	= "$product-$version-$configuration" -replace "\.","_"
+	$artifactsDir	= "$buildDir\Artifacts\$artifactsName"
 	$deploymentDir	= ''
-	
+
 	$buildNumber	= $null
-	
+
 	$setupMessage	= 'Executed Setup!'
 	$cleanMessage	= 'Executed Clean!'
 	$compileMessage	= 'Executed Compile!'
@@ -21,20 +23,37 @@ properties {
 	$deployMessage	= 'Executed Deploy!'
 }
 
-task default -depends Local
+task default -depends Run
 
-task Local {
-	Write-Host "Running local build" -fore Yellow
+task Run {
+	if ($branch -ne $null) {
+		$branch = $branch -replace "refs/heads/",""
+	} else {
+		try { $branch = (git rev-parse --abbrev-ref HEAD) } catch { $branch = 'unknown' }
+	}
+	
+	$script:buildId			= ([guid]::NewGuid().ToString().Substring(0,5))
+	$script:buildVersion	= ?: {$buildNumber -ne $null} {"$version.$buildNumber"} {"$version"}
+	$script:buildLabel		= ?: {$label -ne $null -and $label -ne ''} {"$version-$label"} {"$version"}
+	
+	if ($branch -ne 'master') {
+		$script:buildLabel	= ?: {$buildNumber -ne $null} {"$buildLabel-build$buildNumber"} {"$buildLabel-buildx$buildId"}
+	}
+	
+	# SemVer : 2.0.0-alpha.4+build.3 | 2.0.0-alpha.4
+	# NuGet  : 2.0.0-alpha4-build3 | 2.0.0-alpha4
+	
+	# SemVer : 2.0.0+build.3 | 2.0.0
+	# NuGet  : 2.0.0-build3 | 2.0.0
+
+	Write-Host "Running build" -fore Yellow
 	Write-Host "Product:        $product" -fore Yellow
 	Write-Host "Version:        $version" -fore Yellow
+	Write-Host "Label:          $label" -fore Yellow
 	Write-Host "Build version:  $buildVersion" -fore Yellow
-	Invoke-Task Deploy
-}
-task Release {
-	Write-Host "Running release build" -fore Yellow
-	Write-Host "Product:        $product" -fore Yellow
-	Write-Host "Version:        $version" -fore Yellow
-	Write-Host "Build version:  $buildVersion" -fore Yellow
+	Write-Host "Build label:    $buildLabel" -fore Yellow
+	Write-Host "Branch:         $branch" -fore Yellow
+	
 	Invoke-Task Deploy
 }
 
@@ -63,11 +82,6 @@ task Pack -depends Test {
 }
 
 task Deploy -depends Pack {
-	if ($deploymentDir -ne $null -and $deploymentDir -ne "") {
-		Write-Host "Deploying to: $deploymentDir."
-	} else {
-		Write-Host "No deployment directory set!"
-	}
 	$deployMessage
 }
 
